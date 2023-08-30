6306f5ea81b42f60fcb9789f405984bcff0efdc6
TheRavenousRaven <125660316+TheRavenousRaven@users.noreply.github.com>
2023-08-30 17:31:11 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,34 +1,56 @@
 
 #!/bin/bash
 
-case "$1" in
-    --date)
-        date
-        ;;
-    --logs)
-        if [ $# -eq 1 ]; then
-            for i in {1..100}; do
-                echo "Nazwa pliku: log$i.txt" > "log$i.txt"
-                echo "Skrypt: skrypt.sh" >> "log$i.txt"
-                echo "Data: $(date)" >> "log$i.txt"
-            done
-        elif [ $# -eq 2 ]; then
-            for i in $(seq 1 $2); do
-                echo "Nazwa pliku: log$i.txt" > "log$i.txt"
-                echo "Skrypt: skrypt.sh" >> "log$i.txt"
-                echo "Data: $(date)" >> "log$i.txt"
-            done
-        else
-            echo "Błąd: Nieprawidłowa liczba argumentów."
-        fi
-        ;;
-    --help)
-        echo "Dostępne opcje:"
-        echo "--date: Wyświetla dzisiejszą datę."
-        echo "--logs [liczba]: Tworzy automatycznie pliki logx.txt z numerem i datą."
-        echo "--help: Wyświetla wszystkie dostępne opcje."
-        ;;
-    *)
-        echo "Błąd: Nieznana opcja. Użyj --help, aby zobaczyć dostępne opcje."
-        ;;
-esac
+#Wyświetlanie danych
+display_help() {
+    echo "Usage: skrypt.sh [OPTION] [ARG]"
+    echo "--date, -d              Display today's date"
+    echo "--logs, -l [NUM]        Create log files"
+    echo "--help, -h              Display this help message"
+    echo "--init                  Clone the repository and set PATH"
+    echo "--error, -e [NUM]       Create error files"
+}
+
+#Tworzenie plików
+create_logs() {
+    local num_logs=${1:-100}
+    for ((i=1; i<=$num_logs; i++)); do
+        filename="log${i}.txt"
+        echo "Filename: $filename" > $filename
+        echo "Created by: skrypt.sh" >> $filename
+        echo "Date: $(date)" >> $filename
+    done
+    echo "$num_logs log files created."
+}
+
+#Tworzenie errorów
+create_errors() {
+    local num_errors=${1:-100}
+    mkdir -p errorx
+    for ((i=1; i<=$num_errors; i++)); do
+        filename="errorx/error${i}.txt"
+        echo "Error file ${i}" > $filename
+    done
+    echo "$num_errors error files created."
+}
+
+#Skrypt główny
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+    date
+
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+    create_logs "$2"
+
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    display_help
+
+elif [[ "$1" == "--init" ]]; then
+    git clone <https://github.com/TheRavenousRaven/Narzedzia_Lab_4/.git> Narzedzia_Lab_4
+    export PATH=$PATH:$(pwd)/Narzedzia_Lab_4
+
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    create_errors "$2"
+
+else
+    echo "Invalid option. Use --help to see available options."
+fi
